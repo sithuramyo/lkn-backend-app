2043cd11cc590fb2cd42643c2b069609bbe51ee6
Sithura Myo <66086921+sithuramyo@users.noreply.github.com>
2025-05-31 20:16:58 +0630

    fix: query

--- Domain/Modules/Common/CommonService.cs
+++ Domain/Modules/Common/CommonService.cs
@@ -6,10 +6,9 @@ public class CommonService(AppDbContext context) : ICommonService
 {
     public async Task<ResponseModel<List<MasterCodeItemResponseModel>>> MasterCodeListAsync(MasterCodeItemRequestModel request)
     {
-        List<MasterCodeItemResponseModel> response = [];
         var masterCodeIds = await context.MasterCodes.Where(x => request.Type.Contains(x.Type)).Select(x => x.Id).ToListAsync();
         var masterCodeItems = context.MasterCodeItems
-            .Where(x => masterCodeIds.Contains(x.Id))
+            .Where(x => masterCodeIds.Contains(x.MasterCodeId))
             .Select(x => new MasterCodeItemResponseModel
             {
                 Id = x.Id,
@@ -18,7 +17,6 @@ public class CommonService(AppDbContext context) : ICommonService
                 Description = x.Description ?? string.Empty,
             })
             .ToList();
-        response = masterCodeItems;
-        return ResponseModel<List<MasterCodeItemResponseModel>>.Success(response);
+        return ResponseModel<List<MasterCodeItemResponseModel>>.Success(masterCodeItems);
     }
 }
